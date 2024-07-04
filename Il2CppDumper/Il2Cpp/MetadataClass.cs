f666a7d0531c3d60e49c6c0edef9176361258a2f
tien0246 <60417827+tien0246@users.noreply.github.com>
2024-07-04 20:10:39 +0700

    init

---
+++ Il2CppDumper/Il2Cpp/MetadataClass.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Il2CppDumper
+{
+    public class Il2CppGlobalMetadataHeader
+    {
+        public uint sanity;
+        public int version;
+        public uint stringLiteralOffset;
+        public int stringLiteralSize;
+        public uint stringLiteralDataOffset;
+        public int unknown14;
+        public uint stringOffset;
+        public int unknown1C;
+        public uint exportedTypeDefinitionsOffset;
+        public int attributeDataRangeSize;
+        public uint attributeDataOffset;
+        public int eventsSize;
+        public uint methodsOffset;
+        public int fieldDefaultValuesSize;
+        public uint windowsRuntimeStringsOffset;
+        public int typeDefinitionsSize;
+        public uint fieldDefaultValuesOffset;
+        public int assembliesSize;
+        public uint genericContainersOffset;
+        public int parametersSize;
+        public uint unknown50;
+        public int propertiesSize;
+        public uint propertiesOffset;
+        public int genericParametersSize;
+        public uint parameterDefaultValuesOffset;
+        public int unknown64;
+        public uint attributeDataRangeOffset;
+        public int unknown6C;
+        public uint parametersOffset;
+        public int nestedTypesSize;
+        public uint unknown78;
+        public int vtableMethodsSize;
+        public uint typeDefinitionsOffset;
+        public int methodsSize;
+        public uint unresolvedVirtualCallParameterRangesOffset;
+        public int imagesSize;
+        public uint nestedTypesOffset;
+        public int fieldsSize;
+        public uint fieldRefsOffset;
+        public int unknown9C;
+        public uint genericParameterConstraintsOffset;
+        public int unknownA4;
+        public uint interfaceOffsetsOffset;
+        public int windowsRuntimeTypeNamesSize;
+        public uint windowsRuntimeTypeNamesOffset;
+        public int unknownB4;
+        public uint genericParametersOffset;
+        public int genericContainersSize;
+        public uint vtableMethodsOffset;
+        public int interfacesSize;
+        public uint assembliesOffset;
+        public int fieldRefsSize;
+        public uint eventsOffset;
+        public int unknownD4;
+        public uint imagesOffset;
+        public int genericParameterConstraintsSize;
+        public uint unresolvedVirtualCallParameterTypesOffset;
+        public int unknownE4;
+        public uint interfacesOffset;
+        public int unknownEC;
+        public uint fieldsOffset;
+        public int unknownF4;
+        public uint fieldAndParameterDefaultValueDataOffset;
+        public int parameterDefaultValuesSize;
+    };
+
+    public class Il2CppAssemblyDefinition
+    {
+        public int imageIndex;
+        [Version(Min = 24.1)]
+        public uint token;
+        [Version(Max = 24)]
+        public int customAttributeIndex;
+        [Version(Min = 20)]
+        public int referencedAssemblyStart;
+        [Version(Min = 20)]
+        public int referencedAssemblyCount;
+        public Il2CppAssemblyNameDefinition aname;
+    }
+
+    public class Il2CppAssemblyNameDefinition
+    {
+        public uint nameIndex;
+        public uint cultureIndex;
+        [Version(Max = 24.3)]
+        public int hashValueIndex;
+        public uint publicKeyIndex;
+        public uint hash_alg;
+        public int hash_len;
+        public uint flags;
+        public int major;
+        public int minor;
+        public int build;
+        public int revision;
+        [ArrayLength(Length = 8)]
+        public byte[] public_key_token;
+    }
+
+    public class Il2CppImageDefinition
+    {
+        public uint nameIndex;
+        public int assemblyIndex;
+
+        public int typeStart;
+        public uint typeCount;
+
+        [Version(Min = 24)]
+        public int exportedTypeStart;
+        [Version(Min = 24)]
+        public uint exportedTypeCount;
+
+        public int entryPointIndex;
+        [Version(Min = 19)]
+        public uint token;
+
+        [Version(Min = 24.1)]
+        public int customAttributeStart;
+        [Version(Min = 24.1)]
+        public uint customAttributeCount;
+    }
+
+    public class Il2CppTypeDefinition
+    {
+        public uint nameIndex;
+        public uint namespaceIndex;
+        [Version(Max = 24)]
+        public int customAttributeIndex;
+        public int byvalTypeIndex;
+        [Version(Max = 24.5)]
+        public int byrefTypeIndex;
+
+        public int declaringTypeIndex;
+        public int parentIndex;
+        public int elementTypeIndex; // we can probably remove this one. Only used for enums
+
+        [Version(Max = 24.1)]
+        public int rgctxStartIndex;
+        [Version(Max = 24.1)]
+        public int rgctxCount;
+
+        public int genericContainerIndex;
+
+        [Version(Max = 22)]
+        public int delegateWrapperFromManagedToNativeIndex;
+        [Version(Max = 22)]
+        public int marshalingFunctionsIndex;
+        [Version(Min = 21, Max = 22)]
+        public int ccwFunctionIndex;
+        [Version(Min = 21, Max = 22)]
+        public int guidIndex;
+
+        public uint flags;
+
+        public int fieldStart;
+        public int methodStart;
+        public int eventStart;
+        public int propertyStart;
+        public int nestedTypesStart;
+        public int interfacesStart;
+        public int vtableStart;
+        public int interfaceOffsetsStart;
+
+        public ushort method_count;
+        public ushort property_count;
+        public ushort field_count;
+        public ushort event_count;
+        public ushort nested_type_count;
+        public ushort vtable_count;
+        public ushort interfaces_count;
+        public ushort interface_offsets_count;
+
+        // bitfield to portably encode boolean values as single bits
+        // 01 - valuetype;
+        // 02 - enumtype;
+        // 03 - has_finalize;
+        // 04 - has_cctor;
+        // 05 - is_blittable;
+        // 06 - is_import_or_windows_runtime;
+        // 07-10 - One of nine possible PackingSize values (0, 1, 2, 4, 8, 16, 32, 64, or 128)
+        // 11 - PackingSize is default
+        // 12 - ClassSize is default
+        // 13-16 - One of nine possible PackingSize values (0, 1, 2, 4, 8, 16, 32, 64, or 128) - the specified packing size (even for explicit layouts)
+        public uint bitfield;
+        [Version(Min = 19)]
+        public uint token;
+
+        public bool IsValueType => (bitfield & 0x1) == 1;
+        public bool IsEnum => ((bitfield >> 1) & 0x1) == 1;
+    }
+
+    public class Il2CppMethodDefinition
+    {
+        public uint nameIndex;
+        public int declaringType;
+        public int returnType;
+        public int parameterStart;
+        [Version(Max = 24)]
+        public int customAttributeIndex;
+        public int genericContainerIndex;
+        [Version(Max = 24.1)]
+        public int methodIndex;
+        [Version(Max = 24.1)]
+        public int invokerIndex;
+        [Version(Max = 24.1)]
+        public int delegateWrapperIndex;
+        [Version(Max = 24.1)]
+        public int rgctxStartIndex;
+        [Version(Max = 24.1)]
+        public int rgctxCount;
+        public uint token;
+        public ushort flags;
+        public ushort iflags;
+        public ushort slot;
+        public ushort parameterCount;
+    }
+
+    public class Il2CppParameterDefinition
+    {
+        public uint nameIndex;
+        public uint token;
+        [Version(Max = 24)]
+        public int customAttributeIndex;
+        public int typeIndex;
+    }
+
+    public class Il2CppFieldDefinition
+    {
+        public uint nameIndex;
+        public int typeIndex;
+        [Version(Max = 24)]
+        public int customAttributeIndex;
+        [Version(Min = 19)]
+        public uint token;
+    }
+
+    public class Il2CppFieldDefaultValue
+    {
+        public int fieldIndex;
+        public int typeIndex;
+        public int dataIndex;
+    }
+
+    public class Il2CppPropertyDefinition
+    {
+        public uint nameIndex;
+        public int get;
+        public int set;
+        public uint attrs;
+        [Version(Max = 24)]
+        public int customAttributeIndex;
+        [Version(Min = 19)]
+        public uint token;
+    }
+
+    public class Il2CppCustomAttributeTypeRange
+    {
+        [Version(Min = 24.1)]
+        public uint token;
+        public int start;
+        public int count;
+    }
+
+    public class Il2CppMetadataUsageList
+    {
+        public uint start;
+        public uint count;
+    }
+
+    public class Il2CppMetadataUsagePair
+    {
+        public uint destinationIndex;
+        public uint encodedSourceIndex;
+    }
+
+    public class Il2CppStringLiteral
+    {
+        public uint length;
+        public int dataIndex;
+    }
+
+    public class Il2CppParameterDefaultValue
+    {
+        public int parameterIndex;
+        public int typeIndex;
+        public int dataIndex;
+    }
+
+    public class Il2CppEventDefinition
+    {
+        public uint nameIndex;
+        public int typeIndex;
+        public int add;
+        public int remove;
+        public int raise;
+        [Version(Max = 24)]
+        public int customAttributeIndex;
+        [Version(Min = 19)]
+        public uint token;
+    }
+
+    public class Il2CppGenericContainer
+    {
+        /* index of the generic type definition or the generic method definition corresponding to this container */
+        public int ownerIndex; // either index into Il2CppClass metadata array or Il2CppMethodDefinition array
+        public int type_argc;
+        /* If true, we're a generic method, otherwise a generic type definition. */
+        public int is_method;
+        /* Our type parameters. */
+        public int genericParameterStart;
+    }
+
+    public class Il2CppFieldRef
+    {
+        public int typeIndex;
+        public int fieldIndex; // local offset into type fields
+    }
+
+    public class Il2CppGenericParameter
+    {
+        public int ownerIndex;  /* Type or method this parameter was defined in. */
+        public uint nameIndex;
+        public short constraintsStart;
+        public short constraintsCount;
+        public ushort num;
+        public ushort flags;
+    }
+
+    public enum Il2CppRGCTXDataType
+    {
+        IL2CPP_RGCTX_DATA_INVALID,
+        IL2CPP_RGCTX_DATA_TYPE,
+        IL2CPP_RGCTX_DATA_CLASS,
+        IL2CPP_RGCTX_DATA_METHOD,
+        IL2CPP_RGCTX_DATA_ARRAY,
+        IL2CPP_RGCTX_DATA_CONSTRAINED,
+    }
+
+    public class Il2CppRGCTXDefinitionData
+    {
+        public int rgctxDataDummy;
+        public int methodIndex => rgctxDataDummy;
+        public int typeIndex => rgctxDataDummy;
+    }
+
+    public class Il2CppRGCTXDefinition
+    {
+        public Il2CppRGCTXDataType type => type_post29 == 0 ? (Il2CppRGCTXDataType)type_pre29 : (Il2CppRGCTXDataType)type_post29;
+        [Version(Max = 27.1)]
+        public int type_pre29;
+        [Version(Min = 29)]
+        public ulong type_post29;
+        [Version(Max = 27.1)]
+        public Il2CppRGCTXDefinitionData data;
+        [Version(Min = 27.2)]
+        public ulong _data;
+    }
+
+    public enum Il2CppMetadataUsage
+    {
+        kIl2CppMetadataUsageInvalid,
+        kIl2CppMetadataUsageTypeInfo,
+        kIl2CppMetadataUsageIl2CppType,
+        kIl2CppMetadataUsageMethodDef,
+        kIl2CppMetadataUsageFieldInfo,
+        kIl2CppMetadataUsageStringLiteral,
+        kIl2CppMetadataUsageMethodRef,
+    };
+
+    public class Il2CppCustomAttributeDataRange
+    {
+        public uint token;
+        public uint startOffset;
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Il2CppDumper for Critical Ops
+
+Here is a Unity Il2CppDumper I made that works for Critical Ops version 1.45.4
+
+Tested on iOS.
+
+
+## Requirements
+
+- You need to install [.NET 7.0](https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/runtime-7.0.20-windows-x64-installer) in order to run
+## Usage
+
+Run `Il2CppDumper.exe` and choose the `il2cpp` executable file and `global-metadata.dat` file, then enter the information as prompted
+
+The program will then generate all the output files in current working directory
+
+For more details, check out the credit.
+
+## Credits
+
+ - Perface - [Il2CppDumper](https://github.com/Perfare/Il2CppDumper)
